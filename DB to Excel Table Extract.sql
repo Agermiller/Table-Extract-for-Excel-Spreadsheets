904bddacb45869a006725a7a1fbd7022a2a18954
Austin Germiller <agermiller04@yahoo.com>
2017-03-05 16:12:53 -0700

    Updated

--- DB to Excel Table Extract.sql
+++ DB to Excel Table Extract.sql
@@ -1,4 +1,4 @@
-/*Enter your table name on lines 4 & 12. Edit line 13 for optional ordering.
+/*Enter your table name on lines 4 & 12. Edit line 13 for ordering.
 It is suggested that the SELECT * be modified to return only the necessary columns*/
 
 DECLARE @SIZE AS FLOAT = (SELECT COUNT(1) FROM <Your_Large_Table>)
@@ -10,7 +10,7 @@ WHILE @N < @MAXN
 BEGIN
 	SELECT * 
 	FROM <Your_Large_Table>
-	--ORDER BY <id_column>
+	ORDER BY <id_column>
 	OFFSET POWER(2,20)*@N ROWS
 	FETCH NEXT POWER(2,20) ROWS ONLY
 
