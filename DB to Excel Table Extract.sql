2d252ca76e81726e28d2cd00f6937aaba91ae04e
Austin Germiller <agermiller04@yahoo.com>
2017-03-05 15:54:16 -0700

    Initial Revision

---
+++ DB to Excel Table Extract.sql
@@ -0,0 +1,18 @@
+/*Enter your table name on lines 4 & 12. Edit line 13 for optional ordering.
+It is suggested that the SELECT * be modified to return only the necessary columns*/
+
+DECLARE @SIZE AS FLOAT = (SELECT COUNT(1) FROM <Your_Large_Table>)
+DECLARE @MAXN AS INT = CEILING(@SIZE/POWER(2,20))
+DECLARE @N AS INT = 0
+
+
+WHILE @N < @MAXN
+BEGIN
+	SELECT * 
+	FROM <Your_Large_Table>
+	--ORDER BY <id_column>
+	OFFSET POWER(2,20)*@N ROWS
+	FETCH NEXT POWER(2,20) ROWS ONLY
+
+	SET @N = @N+1
+END
